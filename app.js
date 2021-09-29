06f36249a197104d80dc4e04ce58fa74affaf05a
Ian Rosas <ian.rosasmal@gmail.com>
2021-09-29 13:27:38 -0500

    agregando ruta de bienvenida

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get("/api/v1", (req, res) => {
+app.get("/", (req, res) => {
   res.json([{
     message: "Welcome to API-ene"
   }]);
